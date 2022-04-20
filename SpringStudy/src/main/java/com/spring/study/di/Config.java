b75856d5d0257dfbfee1704e5f909a1db3318401
ITPS <ITPS@gmail.com>
2022-04-20 12:53:39 +0900

    Spring 둘째날

---
+++ SpringStudy/src/main/java/com/spring/study/di/Config.java
@@ -0,0 +1,15 @@
+package com.spring.study.di;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class Config {
+	
+//	private AuthDao authDao;
+//	
+//	@Bean
+//	private AuthService authService() {
+//		return new AuthServiceImpl();
+//	}
+}
